6e326cd4e0d61dc941046a534419713fc5ab913c
Tejash <tejashsinghrajput@gmail.com>
2025-02-09 11:38:21 +0000

    added show route using id

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const mongoose = require("mongoose")
 const listing = require("./models/listing")
 const path = require("path")
 
+
+
 async function main() {
     await mongoose.connect('mongodb://127.0.0.1:27017/highbnb');
 }
@@ -15,6 +17,7 @@ main().then( () => {
 
 app.set("view engine" ,"ejs" );
 app.set("views" , path.join(__dirname, "views"))
+app.use(express.urlencoded ({extended : true}))
 
 app.get("/" , (req,res) => {
     res.send("hi i am groot")
@@ -25,6 +28,13 @@ app.get("/listings",async (req,res) => {
     const alllistings =await listing.find({});
     res.render("listings/index.ejs" ,{alllistings})
 })
+
+//show route
+app.get("/listings/:id", async (req, res) => {
+    let { id } = req.params;
+    const foundListing = await listing.findById(id);
+    res.render("listings/show.ejs", { listing: foundListing });
+});
 // app.get("/testlisting" , async (req,res) => {
 //     let samplelisting = new listing({
 //         title : "my new villa",
